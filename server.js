baa7a190c3dbc0f50c6ab8dd779455e9f966072a
Kevin <kevinvar2805@gmail.com>
2025-02-12 16:11:25 -0600

    Finished setting up routes

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 require('dotenv').config()
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('This is the API for Questions and Answers\nIf you want to look at questions for a certain product\nJust go to /qa for more information or look at the documentation in the ReadMe file')
 })
@@ -12,7 +14,8 @@ app.get('/qa', (req, res) => {
 
 //GET Questions
 app.get('/qa/questions', (req, res) => {
-  if (!req.query.product_id) {
+  var ProductId = req.query.product_id
+  if (!ProductId) {
     return res.status(400).send('Product ID is missing');
   }
   res.send('Success!');
@@ -20,20 +23,54 @@ app.get('/qa/questions', (req, res) => {
 
 //GET Answers
 app.get('/qa/questions/:question_id/answers', (req, res) => {
+  var QuestionId = req.params.question_id;
+  console.log(QuestionId)
   res.send('Success!');
 })
 
 //POST Questions
 app.post('/qa/questions', (req, res) => {
-  if (!req.query.product_id) {
+  var ProductId = req.query.product_id
+  if (!ProductId) {
     return res.status(400).send('Product ID is missing');
+  }
+  if (!req.body || !req.body.body || !req.body.name || !req.body.email) {
+    return res.status(400).send('Missing one or more fields from the body');
   }
   res.sendStatus(201);
 })
 
 //POST Answers
 app.post('/qa/questions/:question_id/answers', (req, res) => {
+  var QuestionId = req.params.question_id;
+  if (!req.body || !req.body.body || !req.body.name || !req.body.email) {
+    return res.status(400).send('Missing one or more fields from the body');
+  }
   res.sendStatus(201);
+})
+
+//Increase helpfulness of Question by 1
+app.put('/qa/questions/:question_id/helpful', (req, res) => {
+  var id = req.params.question_id;
+  res.sendStatus(204);
+})
+
+//Mark question as reported
+app.put('/qa/questions/:question_id/report', (req, res) => {
+  var id = req.params.question_id;
+  res.sendStatus(204);
+})
+
+//Increase helpfulness of Answer by 1
+app.put('/qa/answers/:answer_id/helpful', (req, res) => {
+  var id = req.params.answer_id;
+  res.sendStatus(204);
+})
+
+//Mark answer as reported
+app.put('/qa/answers/:answer_id/report', (req, res) => {
+  var id = req.params.answer_id;
+  res.sendStatus(204);
 })
 
 app.listen(process.env.PORT, () => {
