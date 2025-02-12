3680176165c77f63042c7c5dd39fc1e51e275101
Kevin <kevinvar2805@gmail.com>
2025-02-11 23:43:05 -0600

    got questions to load up

--- extract.js
+++ extract.js
@@ -9,48 +9,58 @@ const photos = './data/answers_photos.csv';
 
 
 
-const questionStream = () => {
+const questionStream = (QuestionModel) => {
   var questionsData = [];
-  return new Promise((resolve, reject) => {
-    fs.createReadStream(questions)
-    .pipe(csv({
-      mapHeaders: ({ header, index }) => {
-        switch (header) {
-          case 'id':
-            return '_id';
-          case 'date_written':
-            return 'createdAt';
-          default:
-            return header;
-        }
-      },
-      mapValues: ({ header, index, value}) => {
-        switch (header) {
-          case '_id':
-          case 'product_id':
-          case 'reported':
-          case 'helpful':
-            return Number(value);
-          case 'createdAt':
-            return new Date(Number(value));
-          default:
-            return value;
-        }
+  fs.createReadStream(questions)
+  .pipe(csv({
+    mapHeaders: ({ header, index }) => {
+      switch (header) {
+        case 'id':
+          return '_id';
+        case 'date_written':
+          return 'createdAt';
+        default:
+          return header;
       }
-    }))
-    .on('data', question => {
-      question['answers'] = {};
-      questionsData.push(question);
-    })
-    .on('error', err => {
-      console.error(err.message);
-    })
-    .on('end', () => {
-      resolve(questionsData);
-      console.log('Questions read successfully');
-    })
+    },
+    mapValues: ({ header, index, value}) => {
+      switch (header) {
+        case '_id':
+        case 'product_id':
+        case 'helpful':
+          return Number(value);
+        case 'reported':
+          return Boolean(value);
+        case 'createdAt':
+          return new Date(Number(value));
+        default:
+          return value;
+      }
+    }
+  }))
+  .on('data', question => {
+    question['answers'] = {};
+    questionsData.push(question);
+    if (questionsData.length === 10000) {
+      console.log('here')
+      var inserted = questionsData.slice();
+      questionsData = [];
+      QuestionModel.insertMany(inserted)
+        .then(() => console.log('successful batch'))
+        .catch((err) => console.error(err))
+    }
+  })
+  .on('error', err => {
+    console.error(err.message);
+  })
+  .on('end', () => {
+    QuestionModel.insertMany(questionsData)
+      .then(() => console.log('successful batch'))
+      .catch((err) => console.error(err))
+    console.log('Questions read successfully');
   })
 }
+
 module.exports.questionStream = questionStream;
   // fs.createReadStream(answers)
   // .pipe(csv())
--- load.js
+++ load.js
@@ -1,9 +1,4 @@
 const QuestionModel = require('./db.js');
 const {questionStream} = require('./extract.js');
 
-const questions = questionStream();
-questions.then(result => QuestionModel.insertMany(result).then(function () {
-  console.log("Data inserted") // Success
-}).catch(function (error) {
-  console.log(error)     // Failure
-}) )
+questionStream(QuestionModel);
