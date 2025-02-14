9765ad3013f766349cf70dab1eb86ffc846562ef
Kevin <kevinvar2805@gmail.com>
2025-02-14 12:02:25 -0600

    Finished tests for get requests

--- server.js
+++ server.js
@@ -10,89 +10,108 @@ app.get('/', (req, res) => {
 })
 
 app.get('/qa', (req, res) => {
-  res.send('To look at questions go to /questions\nTo look at answers go to /answers')
+  res.send('To look at questions go to /qa/questions?product_id=?\nTo look at answers go to /qa/questions/:question_id/answers')
 })
 
 //GET Questions
 app.get('/qa/questions', (req, res) => {
   if (!req.query.product_id) {
-    return res.status(400).send('Product ID is missing');
+    return res.status(400).send('ProductId is missing');
+  }
+  if (isNaN(Number(req.query.product_id)) || Number(req.query.product_id) <= 0 || Number(req.query.product_id) > 4294967295) {
+    return res.status(404).send('Invalid ProductId')
   }
   controller.getQuestions(req.query.product_id, Number(req.query?.page), Number(req.query?.count))
     .then(result => {
       res.send({
-        product_id: req.query.product_id,
+        product_id: Number(req.query.product_id),
         results: transformQuestion(result)
       })
     })
-    .catch(err => res.status(404).send(err))
+    .catch(err => res.status(500).send(err))
 })
 
 //GET Answers
 app.get('/qa/questions/:question_id/answers', (req, res) => {
+  if (isNaN(Number(req.params.question_id)) || req.params.question_id <= 0 || req.params.question_id > 4294967295) {
+    return res.status(404).send('Invalid QuestionId')
+  }
   controller.getAnswers(req.params.question_id, Number(req.query?.page), Number(req.query?.count))
     .then(result =>
       res.send({
-        question: req.params.question_id,
+        question: Number(req.params.question_id),
         page: Number(req.query?.page) ? Number(req.query?.page) : 1,
         count: Number(req.query?.count) ? Number(req.query?.count) : 5,
         results: transformAnswer(result)
       })
     )
-    .catch(err => res.status(404).send(err))
+    .catch(err => res.status(500).send(err))
 })
 
 //POST Questions
 app.post('/qa/questions', (req, res) => {
+  if (isNaN(Number(req.body.product_id)) || req.body.product_id <= 0 || req.body.product_id > 4294967295) {
+    return res.status(404).send('Invalid ProductId')
+  }
   if (!req.body || !req.body.body || !req.body.name || !req.body.email || !req.body.product_id) {
     return res.status(400).send('Missing one or more fields from the body');
   }
   controller.addQuestion(req.body.product_id, req.body.body, req.body.name, req.body.email)
     .then(() => res.sendStatus(201))
-    .catch(err => res.status(404).send(err))
+    .catch(err => res.status(500).send(err))
 })
 
 //POST Answers
 app.post('/qa/questions/:question_id/answers', (req, res) => {
-  var QuestionId = req.params.question_id;
+  if (isNaN(Number(req.params.question_id)) || req.params.question_id <= 0 || req.params.question_id > 4294967295) {
+    return res.status(404).send('Invalid QuestionId')
+  }
   if (!req.body || !req.body.body || !req.body.name || !req.body.email || !req.body.photos) {
     return res.status(400).send('Missing one or more fields from the body');
   }
-  controller.addAnswer(QuestionId, req.body.body, req.body.name, req.body.email, req.body.photos)
+  controller.addAnswer(req.params.question_id, req.body.body, req.body.name, req.body.email, req.body.photos)
     .then(() => res.sendStatus(201))
-    .catch(err => res.status(404).send(err))
+    .catch(err => res.status(500).send(err))
 })
 
 //Increase helpfulness of Question by 1
 app.put('/qa/questions/:question_id/helpful', (req, res) => {
-  var id = req.params.question_id;
-  controller.helpfulQuestion(id)
+  if (isNaN(Number(req.params.question_id)) || req.params.question_id <= 0 || req.params.question_id > 4294967295) {
+    return res.status(404).send('Invalid QuestionId')
+  }
+  controller.helpfulQuestion(req.params.question_id)
     .then(() => res.sendStatus(204))
-    .catch(err => res.status(404).send(err))
+    .catch(err => res.status(500).send(err))
 })
 
 //Mark question as reported
 app.put('/qa/questions/:question_id/report', (req, res) => {
-  var id = req.params.question_id;
-  controller.reportQuestion(id)
+  if (isNaN(Number(req.params.question_id)) || req.params.question_id <= 0 || req.params.question_id > 4294967295) {
+    return res.status(404).send('Invalid QuestionId')
+  }
+  controller.reportQuestion(req.params.question_id)
     .then(() => res.sendStatus(204))
-    .catch(err => res.status(404).send(err))
+    .catch(err => res.status(500).send(err))
 })
 
 //Increase helpfulness of Answer by 1
 app.put('/qa/answers/:answer_id/helpful', (req, res) => {
-  var id = req.params.answer_id;
-  controller.helpfulAnswer(id)
+  if (isNaN(Number(req.params.answer_id)) || req.params.answer_id <= 0 || req.params.answer_id > 4294967295) {
+    return res.status(404).send('Invalid AnswerId')
+  }
+  controller.helpfulAnswer(req.params.answer_id)
     .then(() => res.sendStatus(204))
-    .catch(err => res.status(404).send(err))
+    .catch(err => res.status(500).send(err))
 })
 
 //Mark answer as reported
 app.put('/qa/answers/:answer_id/report', (req, res) => {
-  var id = req.params.answer_id;
-  controller.reportAnswer(id)
+  if (isNaN(Number(req.params.answer_id)) || req.params.answer_id <= 0 || req.params.answer_id > 4294967295) {
+    return res.status(404).send('Invalid AnswerId')
+  }
+  controller.reportAnswer(req.params.answer_id)
     .then(() => res.sendStatus(204))
-    .catch(err => res.status(404).send(err))
+    .catch(err => res.status(500).send(err))
 })
 
 module.exports = app.listen(process.env.PORT, () => {
--- test.js
+++ test.js
@@ -5,7 +5,7 @@ var expect = require('chai').expect;
 describe('Testing API Calls', () => {
 
   describe('GET /', () => {
-    it('should return 200 OK', () => {
+    it('should return 200 OK and text to direct you in the right direction', () => {
       return request(app)
         .get('/')
         .set('Accept', 'text/html')
@@ -13,19 +13,146 @@ describe('Testing API Calls', () => {
         .expect(200)
         .then(response => {
           expect(response.text).to.equal('This is the API for Questions and Answers\nIf you want to look at questions for a certain product\nJust go to /qa for more information or look at the documentation in the ReadMe file');
-        })
+        });
     });
   });
 
   describe('GET /qa', () => {
-    return request(app)
-      .get('/qa')
-      .set('Accept', 'text/html')
-      .expect('Content-Type', /text/)
-      .expect(200)
-      .then(response => {
-        expect(response.text).to.equal('To look at questions go to /questions\nTo look at answers go to /answers');
-      })
-  })
+    it('should return 200 OK and text to direct you in the right direction', () => {
+      return request(app)
+        .get('/qa')
+        .set('Accept', 'text/html')
+        .expect('Content-Type', /text/)
+        .expect(200)
+        .then(response => {
+          expect(response.text).to.equal('To look at questions go to /qa/questions?product_id=?\nTo look at answers go to /qa/questions/:question_id/answers');
+        });
+    });
+  });
 
+  describe('GET /qa/questions', () => {
+    it('Should get all the questions at product id 1 and they should be formatted correctly', () => {
+      return request(app)
+        .get('/qa/questions?product_id=1')
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .then(response => {
+          expect(response.body.product_id).to.equal(1);
+          expect(response.body.results.length).to.not.equal(0);
+          expect(response.body.results[0].question_id).to.be.a('number');
+          expect(response.body.results[0].question_body).to.be.a('string');
+          expect(!isNaN(Date.parse(response.body.results[0].question_date))).to.be.true;
+          expect(response.body.results[0].asker_name).to.be.a('string');
+          expect(response.body.results[0].question_helpfulness).to.be.a('number');
+          expect(response.body.results[0].reported).to.be.false;
+          expect(response.body.results[0].answers).to.be.a('object');
+          expect(response.body.results[0].answers[Object.keys(response.body.results[0].answers)[Object.keys(response.body.results[0].answers).length - 1]].id).to.equal(Number(Object.keys(response.body.results[0].answers)[Object.keys(response.body.results[0].answers).length - 1]))
+          expect(response.body.results[0].answers[Object.keys(response.body.results[0].answers)[Object.keys(response.body.results[0].answers).length - 1]].body).to.be.a('string');
+          expect(!isNaN(Date.parse(response.body.results[0].answers[Object.keys(response.body.results[0].answers)[Object.keys(response.body.results[0].answers).length - 1]].date))).to.be.true;
+          expect(response.body.results[0].answers[Object.keys(response.body.results[0].answers)[Object.keys(response.body.results[0].answers).length - 1]].answerer_name).to.be.a('string');
+          expect(response.body.results[0].answers[Object.keys(response.body.results[0].answers)[Object.keys(response.body.results[0].answers).length - 1]].helpfulness).to.be.a('number');
+          expect(response.body.results[0].answers[Object.keys(response.body.results[0].answers)[Object.keys(response.body.results[0].answers).length - 1]].photos).to.be.a('array');
+          expect(response.body.results[0].answers[Object.keys(response.body.results[0].answers)[Object.keys(response.body.results[0].answers).length - 1]].photos[0].id).to.be.a('number');
+          expect(response.body.results[0].answers[Object.keys(response.body.results[0].answers)[Object.keys(response.body.results[0].answers).length - 1]].photos[0].url).to.be.a('string');
+        })
+    });
+
+    it('Should throw an error if given product id is invalid', () => {
+      return request(app)
+        .get('/qa/questions?product_id=0')
+        .set('Accept', 'text/html')
+        .expect('Content-Type', /text/)
+        .expect(404)
+        .then((err) => {
+          expect(err.text).to.equal('Invalid ProductId');
+          return request(app)
+            .get('/qa/questions?product_id=4294967296')
+            .set('Accept', 'text/html')
+            .expect('Content-Type', /text/)
+            .expect(404)
+            .then((err) => {
+              expect(err.text).to.equal('Invalid ProductId');
+              return request(app)
+                .get('/qa/questions?product_id=fhasfhdsj')
+                .set('Accept', 'text/html')
+                .expect('Content-Type', /text/)
+                .expect(404)
+                .then((err) => {
+                  expect(err.text).to.equal('Invalid ProductId')
+                })
+            })
+        })
+    });
+
+    it('Should throw an error if given no product id', () => {
+      return request(app)
+        .get('/qa/questions')
+        .set('Accept', 'text/html')
+        .expect('Content-Type', /text/)
+        .expect(400)
+        .then((err) => {
+          expect(err.text).to.equal('ProductId is missing');
+        })
+    });
+  });
+
+  describe('GET /qa/questions/:question_id/answers', () => {
+
+    it('Should get all the answers at question id 1 and they should be formatted correctly', () => {
+      return request(app)
+        .get('/qa/questions/1/answers')
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .then(response => {
+          expect(response.body.question).to.equal(1);
+          expect(response.body.page).to.equal(1);
+          expect(response.body.count).to.equal(5);
+          expect(response.body.results.length).to.not.equal(0);
+          expect(response.body.results[0].answer_id).to.be.a('number');
+          expect(response.body.results[0].body).to.be.a('string');
+          expect(!isNaN(Date.parse(response.body.results[0].date))).to.be.true;
+          expect(response.body.results[0].answerer_name).to.be.a('string');
+          expect(response.body.results[0].helpfulness).to.be.a('number');
+          expect(response.body.results[0].photos).to.be.a('array');
+          expect(response.body.results[3].photos[0].id).to.be.a('number');
+          expect(response.body.results[3].photos[0].url).to.be.a('string');
+        })
+    });
+
+    it('Should throw an error if given question id is invalid', () => {
+      return request(app)
+        .get('/qa/questions/0/answers')
+        .set('Accept', 'text/html')
+        .expect('Content-Type', /text/)
+        .expect(404)
+        .then((err) => {
+          expect(err.text).to.equal('Invalid QuestionId');
+          return request(app)
+            .get('/qa/questions/4294967296/answers')
+            .set('Accept', 'text/html')
+            .expect('Content-Type', /text/)
+            .expect(404)
+            .then((err) => {
+              expect(err.text).to.equal('Invalid QuestionId');
+              return request(app)
+                .get('/qa/questions/dfsdfjdj/answers')
+                .set('Accept', 'text/html')
+                .expect('Content-Type', /text/)
+                .expect(404)
+                .then((err) => {
+                  expect(err.text).to.equal('Invalid QuestionId')
+                })
+            })
+        })
+    });
+
+  });
+
+  describe('POST /qa/questions', () => {
+    it('Should create a question if given valid fields', () => {
+
+    })
+  })
 });
